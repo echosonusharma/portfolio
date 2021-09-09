94d1e9c3841b6e0d90f0f65ddea51a97b42b73c0
sonu sharma <echosonusharma@gmail.com>
2021-09-10 03:43:54 +0530

    resume timeline template added

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,23 +1,71 @@
 import React from "react";
 import { Box, Center, useMediaQuery } from "@chakra-ui/react";
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+
 
 const Resume = () => {
     const [lg] = useMediaQuery("(min-width: 1280px)");
 
     return (
-        <Box>
-            <Center
-                fontFamily='Monoton'
-                fontWeight="400"
-                fontSize={['4xl', '6xl']}
-                py={['1rem', '3rem', '5rem']}
-            >RESUME</Center>
+        <>
             <Box>
-                <Center>
-                    <div style={{ height: '50rem', width: '0.1rem', backgroundColor: '#ff0080' }} />
-                </Center>
+                <Center
+                    fontFamily='Monoton'
+                    fontWeight="400"
+                    fontSize={['4xl', '6xl']}
+                    py={['1rem', '3rem', '5rem']}
+                >RESUME</Center>
             </Box>
-        </Box>
+            <div>
+                <VerticalTimeline>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                        contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+                        date="2011 - present"
+                        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                    >
+                        <h3 className="vertical-timeline-element-title">Creative Director</h3>
+                        <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+                        <p>Creative Direction, User Experience, Visual Design, Project Management, Team Leading</p>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                        contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+                        date="2010 - 2011"
+                        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                    >
+                        <h3 className="vertical-timeline-element-title">Art Director</h3>
+                        <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+                        <p>Creative Direction, User Experience, Visual Design, SEO, Online Marketing</p>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                        contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+                        date="2008 - 2010"
+                        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                    >
+                        <h3 className="vertical-timeline-element-title">Web Designer</h3>
+                        <h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
+                        <p> User Experience, Visual Design</p>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                        contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+                        date="2006 - 2008"
+                        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                    >
+                        <h3 className="vertical-timeline-element-title">Web Designer</h3>
+                        <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+                        <p>User Experience, Visual Design</p>
+                    </VerticalTimelineElement>
+                </VerticalTimeline>
+            </div>
+        </>
     )
 };
 
